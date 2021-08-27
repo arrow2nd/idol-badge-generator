5f69b9e99f13e9e9702909c011a707c9dabed660
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-08-27 11:51:50 +0900

    add: Support for idols with no personal color

--- hooks/useIdolData.ts
+++ hooks/useIdolData.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { Idol } from '../types/idol'
+import { BlandColor } from '../data/bland-color'
 
 export const useIdolData = (name: string): Idol | undefined => {
   const [idolData, setIdolData] = useState<Idol>()
@@ -12,15 +13,16 @@ export const useIdolData = (name: string): Idol | undefined => {
     PREFIX imas: <https://sparql.crssnky.xyz/imasrdf/URIs/imas-schema.ttl#>
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     
-    SELECT DISTINCT ?name ?hex ?url
+    SELECT DISTINCT ?name ?bland ?hex ?url
     
     WHERE {
       ?d rdf:type ?type;
-           rdfs:label ?name;
-           imas:Color ?hex.
-    OPTIONAL { ?d imas:IdolListURL ?url}.
-    FILTER(CONTAINS(?name, "${name}")).
-    }  
+         imas:Brand ?bland;
+         rdfs:label ?name.
+      FILTER(CONTAINS(?name, "${name}"))
+      OPTIONAL { ?d imas:Color ?hex.}
+      OPTIONAL { ?d imas:IdolListURL ?url}
+    }
     `
 
     const url = `https://sparql.crssnky.xyz/spql/imas/query?output=json&query=${encodeURIComponent(
@@ -31,10 +33,14 @@ export const useIdolData = (name: string): Idol | undefined => {
       .then((res) => res.json())
       .then((json) => {
         const binding = json.results.bindings[0]
+        const bland: string = binding.bland.value
+        const hex: string = binding?.hex
+          ? binding.hex.value
+          : BlandColor[bland].hex
 
         const results: Idol = {
           name: binding.name.value,
-          hex: binding.hex.value,
+          hex: hex,
           url: binding.url?.value || 'https://idolmaster-official.jp/'
         }
 
