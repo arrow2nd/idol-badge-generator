8c9000134a779865654450ac43345aa292b6881e
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-11-25 17:18:04 +0900

    add: HTML

--- components/result/index.tsx
+++ components/result/index.tsx
@@ -13,12 +13,14 @@ const Results = ({ idolData, style }: Props) => {
   const paramStyle = style ? `?style=${style}` : ''
 
   const url = `https://img.shields.io/badge/${paramLabel}-${paramName}-${idolData.hex}${paramStyle}`
+  const html = `<a href="${idolData.url}"><img src="${url}" alt="${idolData.name}" title="${idolData.name}"></a>`
   const markdown = `[![${idolData.name}](${url})](${idolData.url})`
 
   return (
     <div className="flex flex-col items-center">
       <Badge url={url} />
       <Input label="URL" text={url} />
+      <Input label="HTML" text={html} />
       <Input label="Markdown" text={markdown} />
     </div>
   )
