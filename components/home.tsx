55f3c8a03c9f3fce4c6b01f3389a7fb8d06f046a
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-09-20 13:55:41 +0900

    feat(data/site): サイトタイトルを変更

--- components/home.tsx
+++ components/home.tsx
@@ -4,24 +4,28 @@ import Header from './header'
 import UI from './ui'
 import Footer from './footer'
 
-const Home = () => (
-  <div className="p-10">
-    <Head>
-      <title>{site.title}</title>
-      <meta name="description" content={site.desc} />
-      <meta property="og:url" content={site.url} />
-      <meta property="og:title" content={site.title} />
-      <meta property="og:description" content={site.desc} />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:creator" content="@arrow_2nd" />
-      <meta name="twitter:url" content={site.url} />
-      <meta name="twitter:title" content={site.title} />
-      <meta name="twitter:description" content={site.desc} />
-    </Head>
-    <Header />
-    <UI />
-    <Footer />
-  </div>
-)
+const Home = () => {
+  const { title, desc, url } = site
+
+  return (
+    <div className="p-10">
+      <Head>
+        <title>{`${title} | ${desc}`}</title>
+        <meta name="description" content={desc} />
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={desc} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="@arrow_2nd" />
+        <meta name="twitter:url" content={url} />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={desc} />
+      </Head>
+      <Header />
+      <UI />
+      <Footer />
+    </div>
+  )
+}
 
 export default Home
